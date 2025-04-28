6ded910242a522842116413667cfbdfd09e3a207
g00415754 <114396637+g00415754@users.noreply.github.com>
2025-04-28 20:49:54 +0100

    Update about-the-band.php

--- about-the-band.php
+++ about-the-band.php
@@ -94,7 +94,7 @@
         <div class="row">
             <?php
             // Database connection
-            $conn = new mysqli('localhost', 'root', 'root', 'indieband_db');
+            $conn = new mysqli('indieband', 'root', '', 'indieband_db');
             if ($conn->connect_error) {
                 die('Connection failed: ' . $conn->connect_error);
             }
